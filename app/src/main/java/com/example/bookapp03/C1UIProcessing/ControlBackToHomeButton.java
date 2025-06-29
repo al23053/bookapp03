a257e8786fac00aa8b3b0a846bec97aec854e7c2
whalelab12 <jobugorira211@gmail.com>
2025-06-29 16:04:29 +0900

    Manifestと少し修正

--- app/src/main/java/com/example/bookapp03/C1UIProcessing/ControlBackToHomeButton.java
+++ app/src/main/java/com/example/bookapp03/C1UIProcessing/ControlBackToHomeButton.java
@@ -1,38 +1,62 @@
 package com.example.bookapp03.C1UIProcessing;
 
-import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
 import android.widget.ImageButton;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 /**
- * モジュール名: ホームへ戻るボタン制御
+ * モジュール名: ホーム画面戻りボタン制御
  * 作成者: 鶴田凌
- * 作成日: 2025/06/15
- * 概要: 戻るボタンを押したときにアクティビティを終了するクラス
+ * 作成日: 2025/06/16
+ * 概要: 戻るボタンが押されたときにホーム画面に遷移する制御クラス
  * 履歴:
- * 　2025/06/15 鶴田凌 新規作成
+ * 2025/06/16 鶴田凌 新規作成
  */
 public class ControlBackToHomeButton {
-
-    /**
-     * Activity を保持し、finish() を呼び出すための参照
-     */
-    private final Activity activity;
+    
+    private static final String TAG = "ControlBackToHomeButton";
+    
+    private final AppCompatActivity activity;
 
     /**
      * コンストラクタ
      *
-     * @param activity このコントローラを利用する Activity
+     * @param activity 呼び出し元Activity
      */
-    public ControlBackToHomeButton(Activity activity) {
+    public ControlBackToHomeButton(AppCompatActivity activity) {
         this.activity = activity;
     }
 
     /**
-     * 指定した ImageButton を押下したときに Activity.finish() を実行するリスナーを設定する。
+     * 戻るボタンにイベントリスナーを設定
      *
-     * @param btnBack 戻るボタンとして機能させる ImageButton
+     * @param btnBack 戻るボタン
      */
     public void bind(ImageButton btnBack) {
-        btnBack.setOnClickListener(v -> activity.finish());
+        if (btnBack != null) {
+            btnBack.setOnClickListener(v -> navigateToHome());
+        } else {
+            Log.e(TAG, "戻るボタンが null です");
+        }
+    }
+
+    /**
+     * ホーム画面に遷移
+     */
+    private void navigateToHome() {
+        try {
+            Log.d(TAG, "ホーム画面に戻ります");
+            
+            Intent intent = new Intent(activity, DisplayHome.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            
+            activity.startActivity(intent);
+            activity.finish();
+            
+        } catch (Exception e) {
+            Log.e(TAG, "ホーム画面への遷移に失敗", e);
+        }
     }
 }
